2844f4fb62d656054fcc965dab777b6b2e29ea82
Daniel Fagundes <fdanielvicente@gmail.com>
2024-06-08 03:38:18 -0300

    meu projeto conta banco

---
+++ tipos-variaveis/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "DANIEL FAFUNDES";
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+
+         System.out.println("Hello, World!");
+    
+    }
+
+}
+
+
+
+
+
+
